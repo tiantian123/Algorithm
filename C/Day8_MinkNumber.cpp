c641efa539f102020a1126c5db6976c9daa6c010
chentian123 <2294300507@qq.com>
2020-03-21 00:31:46 +0800

    Min k Number

---
+++ C/Day8_MinkNumber.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> getLeastNumbers(vector<int>& arr, int k) {
+        vector<int> vec(k, 0);
+        if (k == 0) return vec;
+        priority_queue<int> Q;
+        for (int i = 0; i < k; ++i) Q.push(arr[i]);
+        for (int i = k; i < arr.size(); i++) {
+            if (Q.top() > arr[i]) {
+                Q.pop();
+                Q.push(arr[i]);
+            }
+        }
+        for (int i=0; i < k; i++) {
+            vec[i] = Q.top();
+            Q.pop();
+        }
+        return vec;
+    }
+};
