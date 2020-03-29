330e349b5acfc890a304c70fb4a1e815206663f0
chentian123 <2294300507@qq.com>
2020-03-29 18:52:04 +0800

    102. Binary Tree level  order, add DFS solution

--- Python/LeetCodePractice/Day16_102_BTLevelOrder.py
+++ Python/LeetCodePractice/Day16_102_BTLevelOrder.py
@@ -16,8 +16,9 @@ class TreeNode:
         self.left = None
         self.right = None
 
-class Solution:
+class BFS_Solution:
     def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        """ approach1: BFS"""
         if not root:
             return []
         result = []
@@ -36,3 +37,22 @@ class Solution:
                 if node.right: queue.append(node.right)
             result.append(current_level)
         return result
+
+
+class DfsSolution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+        self.result = []
+        self._dfs(root, 0)
+        return self.result
+
+    def _dfs(self, node: TreeNode, level: int):
+        if not node:
+            return None
+        if len(self.result) < level + 1:
+            self.result.append([])
+
+        self.result[level].append(node.val)
+        self._dfs(node.left, level + 1)
+        self._dfs(node.right, level + 1)
